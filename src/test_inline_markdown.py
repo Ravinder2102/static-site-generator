01f327501eb0424e2baef8a134842596d5126d8e
Ravinder <124263446+Ravinder2102@users.noreply.github.com>
2026-07-02 22:48:23 +0530

    added tests for markdown_to_blocks function

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -7,7 +7,8 @@ from inline_markdown import (
     extract_markdown_links,
     split_nodes_image,
     split_nodes_link,
-    text_to_textnodes
+    text_to_textnodes,
+    markdown_to_blocks
 )
 class TestInlineMarkdown(unittest.TestCase):
     def test_code_block(self):
@@ -116,4 +117,57 @@ class TestInlineMarkdown(unittest.TestCase):
             nodes
         )
 
+        def test_markdown_to_blocks(self):
+            md = """
+    This is **bolded** paragraph
+
+    This is another paragraph with _italic_ text and `code` here
+    This is the same paragraph on a new line
+
+    - This is a list
+    - with items
+    """
+            blocks = markdown_to_blocks(md)
+            self.assertEqual(
+                blocks,
+                [
+                    "This is **bolded** paragraph",
+                    "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
+                    "- This is a list\n- with items",
+                ],
+        )
+            
+
+        def test_markdown_to_blocks_with_empty_extra_lines(self):
+            md = """
+
+
+    This is **bolded** paragraph
+
     
+
+
+
+
+    This is another paragraph with _italic_ text and `code` here
+    This is the same paragraph on a new line
+
+    
+
+
+    - This is a list
+    - with items
+
+
+
+
+    """
+            blocks = markdown_to_blocks(md)
+            self.assertEqual(
+                blocks,
+                [
+                    "This is **bolded** paragraph",
+                    "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
+                    "- This is a list\n- with items",
+                ],
+        )
