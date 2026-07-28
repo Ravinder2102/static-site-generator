ddee6d16a3a75fc164b6c4c45e54538a1d3f40e2
Ravinder <124263446+Ravinder2102@users.noreply.github.com>
2026-07-28 20:44:02 +0530

    fixed the bug where the tests for markdown_to_blocks were indented wrong and also fixed the test failure where the empty lines had whitespace in them

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -117,57 +117,49 @@ class TestInlineMarkdown(unittest.TestCase):
             nodes
         )
 
-        def test_markdown_to_blocks(self):
-            md = """
-    This is **bolded** paragraph
+    def test_markdown_to_blocks(self):
+        md = """
+This is **bolded** paragraph
 
-    This is another paragraph with _italic_ text and `code` here
-    This is the same paragraph on a new line
+This is another paragraph with _italic_ text and `code` here
+This is the same paragraph on a new line
 
-    - This is a list
-    - with items
-    """
-            blocks = markdown_to_blocks(md)
-            self.assertEqual(
-                blocks,
-                [
-                    "This is **bolded** paragraph",
-                    "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
-                    "- This is a list\n- with items",
-                ],
+- This is a list
+- with items
+"""
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            [
+                "This is **bolded** paragraph",
+                "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
+                "- This is a list\n- with items",
+            ],
         )
             
 
-        def test_markdown_to_blocks_with_empty_extra_lines(self):
-            md = """
-
-
-    This is **bolded** paragraph
-
-    
+    def test_markdown_to_blocks_with_empty_extra_lines(self):
+        md = """
+This is **bolded** paragraph
 
 
 
 
-    This is another paragraph with _italic_ text and `code` here
-    This is the same paragraph on a new line
-
-    
-
-
-    - This is a list
-    - with items
+This is another paragraph with _italic_ text and `code` here
+This is the same paragraph on a new line
 
 
 
 
-    """
-            blocks = markdown_to_blocks(md)
-            self.assertEqual(
-                blocks,
-                [
-                    "This is **bolded** paragraph",
-                    "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
-                    "- This is a list\n- with items",
-                ],
+- This is a list
+- with items
+"""
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            [
+                "This is **bolded** paragraph",
+                "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
+                "- This is a list\n- with items",
+            ],
         )
