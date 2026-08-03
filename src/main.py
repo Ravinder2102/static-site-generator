e6b08c131f88fe90183a75f41fa2f09294810937
Ravinder <124263446+Ravinder2102@users.noreply.github.com>
2026-08-03 22:03:48 +0530

    Added generate_page functionality with required paths

--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
 from copy_static import copy_static, delete_destination
+from generate_pages import generate_page
 
 dir_path_static = "./static"
 dir_path_public = "./public"
+content_path = "./content/index.md"
+template_path = "./template.html"
+index_path = "./public/index.html"
 
 def main():
     delete_destination(dir_path_public)
     copy_static(dir_path_static, dir_path_public)
-
+    generate_page(content_path, template_path, index_path)
 main()
